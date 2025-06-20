3fb55e1ab1e410024add1e0a5e833eebf8475320
Anshuman Garg <anshumangarg5410@gmail.com>
2025-06-20 11:15:06 +0530

    appwrite

---
+++ src/conf/config.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwritecollectionId: String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBucket_Id: String(import.meta.env.VITE_BUCKET_ID)
+}
+
+export default conf
